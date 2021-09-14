15e3999827cf42a4b88f4988c6d40ae5a8208e38
SeanYang816 <64162342+SeanYang816@users.noreply.github.com>
2021-09-14 10:39:09 +0800

    Update ReservationCard.js

--- src/components/ReservationCard.js
+++ src/components/ReservationCard.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { removeReservation } from '../features/reservationSlice'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeReservation } from "../features/reservationSlice";
+import { addCustomer } from "../features/customerSlice";
+import { v4 as uuid } from "uuid";
 
-function ReservationCard({reservationName, index}) {
-
-    const dispatch = useDispatch()
-    return (
-        <div onClick={()=> dispatch(removeReservation(index))} >
-              <div className="reservation-card-container">{reservationName}</div>
-
-        </div>
-    )
+function ReservationCard({ reservationName, index }) {
+  const dispatch = useDispatch();
+  return (
+    <div
+      onClick={() => {
+        dispatch(removeReservation(index));
+        dispatch(
+          addCustomer({
+            id: uuid(),
+            name: reservationName,
+            food: [],
+          })
+        );
+      }}
+    >
+      <div className="reservation-card-container">{reservationName}</div>
+    </div>
+  );
 }
 
-export default ReservationCard
+export default ReservationCard;
