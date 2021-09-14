d10e57f64c5ab4e7373efbcacd355b7ccec8a473
SeanYang816 <64162342+SeanYang816@users.noreply.github.com>
2021-09-14 10:38:07 +0800

    finish project

--- src/features/customerSlice.js
+++ src/features/customerSlice.js
@@ -1,18 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const initialState = {
-    value: []
-}
+  value: [],
+};
 
 export const customerSlice = createSlice({
-    name: "customers",
-    initialState,
-    reducers: {
+  name: "customers",
+  initialState,
+  reducers: {
+    addCustomer: (state, action) => {
+      state.value.push(action.payload);
+    },
+    addFoodToCustomer: (state, action) => {
+      state.value.forEach((customer) => {
+        if (customer.id === action.payload.id) {
+          customer.food.push(action.payload.food);
+        }
+      });
+    },
+  },
+});
 
-    }
-})
-
-export const {} = customerSlice.actions;
+export const { addCustomer, addFoodToCustomer } = customerSlice.actions;
 
 export default customerSlice.reducer;
