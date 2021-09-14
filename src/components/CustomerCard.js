ae46a7682fc023dbeedd5db324f4f27a1593e8ad
SeanYang816 <64162342+SeanYang816@users.noreply.github.com>
2021-09-14 10:38:29 +0800

    finish project

--- src/components/CustomerCard.js
+++ src/components/CustomerCard.js
@@ -1,20 +1,44 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { addFoodToCustomer } from "../features/customerSlice";
 
-function CustomerCard() {
-    return (
-        <div className="customer-food-container">
-          <div className="customer-food-card-container">
-            <p>Selena Gomez</p>
-            <div className="customer-foods-container">
-              <div className="customer-food"></div>
-              <div className="customer-food-input-container">
-                <input/>
-                <button>Add</button>
-              </div>
-            </div>
-          </div>
+function CustomerCard({ id, name, food }) {
+  const dispatch = useDispatch();
+  const [customerInput, setCustomerInput] = useState("");
+
+  return (
+    <div className="customer-food-card-container">
+      <p>{name}</p>
+      <div className="customer-foods-container">
+        <div className="customer-food">
+          {food.map((food, index) => {
+            return <p key={index}>{food}</p>;
+          })}
         </div>
-    )
+        <div className="customer-food-input-container">
+          <input
+            value={customerInput}
+            onChange={(e) => setCustomerInput(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              if (!customerInput) return;
+              dispatch(
+                addFoodToCustomer({
+                  id,
+                  food: customerInput,
+                })
+              );
+              setCustomerInput("");
+            }}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CustomerCard
+export default CustomerCard;
